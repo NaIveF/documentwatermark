a97efad1ca42d62a59515d28c673baa6a386dde0
NaIve <3505708329@qq.com>
2020-07-21 22:34:53 +0800

    finish judge difference function

--- InvisibleWatermark4txtFile.py
+++ InvisibleWatermark4txtFile.py
@@ -4,21 +4,22 @@ import docx2txt
 
 def string_to_md5(string):
     md5_val = hashlib.md5(string.encode('utf8')).hexdigest()
-
     return md5_val
+
 
 def binary_replace(binary_str):
     temp = []
     for i in range(0, len(binary_str), 2):
         if binary_str[i:i + 2] == '00':
-            temp.append('\u202A\u202C')
+            temp.append('\u202A')
         elif binary_str[i:i + 2] == '01':
-            temp.append('\u202B\u202C')
+            temp.append('\u202B')
         elif binary_str[i:i + 2] == '11':
-            temp.append('\u202D\u202C')
+            temp.append('\u202C')
         elif binary_str[i:i + 2] == '10':
-            temp.append('\u202E\u202C')
+            temp.append('\u202D')
     return ''.join(temp)
+
 
 def calc_crc(string):
     data = bytearray.fromhex(string)
@@ -34,9 +35,9 @@ def calc_crc(string):
     return hex(((crc & 0xff) << 8) + (crc >> 8))
 
 
-if __name__ == "__main__":
-    fin = open('text.txt','r',encoding='utf-8')  # read file
-    fout = open('result.txt','w',encoding='utf-8')
+def add_watermark(filename_origin, filename_target):
+    fin = open(filename_origin, 'r', encoding='utf-8')  # read file
+    fout = open(filename_target, 'w', encoding='utf-8')
     my_text = fin.readlines()
     fin.close()
     result = []
@@ -44,8 +45,8 @@ if __name__ == "__main__":
         list = paragraph.split('。')  # split data depend on '。'
         temp = []
         for sentence in list:  # add watermark each line
-            print('[*]The string after add watermark: ' + sentence)
-            print('[*]The string size after add watermark: ' + str(len(sentence)))
+            print('[*]The string before add watermark: ' + sentence)
+            print('[*]The string size before add watermark: ' + str(len(sentence)))
             md5 = string_to_md5(sentence)  # generate md5 string
             crc = calc_crc(md5)  # generate crc string with md5 string
             binary = bin(int(crc, 16))[2:]  # get crc binary string
@@ -61,3 +62,30 @@ if __name__ == "__main__":
     fout.close()
 
 
+def judge_different(file_target):
+    ft = open(file_target, 'r', encoding='utf-8')
+    alllines_ft = ft.readlines()
+    for paragraph in alllines_ft:
+        list = paragraph.split('。')  # split data depend on '。'
+        for sentence in list:  # add watermark each line
+            if len(sentence) == 1:
+                continue
+            str_origin = sentence[:-8]
+            str_watermark = sentence[-8:]
+            md5 = string_to_md5(str_origin)  # generate md5 string
+            crc = calc_crc(md5)  # generate crc string with md5 string
+            binary = bin(int(crc, 16))[2:]  # get crc binary string
+            for i in range(len(binary), 16):
+                binary = '0' + binary
+            binary_result = binary_replace(binary)  # replace crc string as invisible unicode
+            if binary_result != str_watermark:
+                print(
+                    f'[-]The target file is different from  origin file in: paragraph {alllines_ft.index(paragraph) + 1} sentence {list.index(sentence) + 1}. ' + sentence+'。')
+    ft.close()
+
+
+if __name__ == "__main__":
+    print('***************************\nAdd watermark to txt file.\n***************************')
+    add_watermark('text.txt', 'result.txt')
+    print('***************************\nCheck any change in txt file.\n***************************')
+    judge_different('result1.txt')  # some difference from generated file
